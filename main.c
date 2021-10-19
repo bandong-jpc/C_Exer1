5331823c9606fd845e3d18467c06a5c98e48f001
bandong-jpc <bandong.jpc@gmail.com>
2021-10-19 12:04:14 +0800

    Add user input to prompt function

--- main.c
+++ main.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 
 void prompt(){
+  int quarters = 0, dimes = 0, nickels = 0, pennies = 0;
+ 
   printf("\nInput # of quarters: ");
+  scanf("%d", &quarters);
   printf("\nInput # of dimes: ");
+  scanf("%d", &dimes);
   printf("\nInput # of nickels: ");
+  scanf("%d", &nickels);
   printf("\nInput # of pennies: ");
+  scanf("%d", &pennies);
+
 }
 
 int main(){
